07aa5f29a95556af4865688e030b944716e97059
Manning Wu <manningwu07@users.noreply.github.com>
2025-10-29 22:54:25 -0400

    Revert "Added demo auth----doesnt exactly work/isnt tested but the pages are there"
    This reverts commit 276f5d3bc34ff28f6e82547a8535c7087fa9faa7.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "~/styles/globals.css";
-import { AuthGuard } from "./components/AuthGuard";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +27,7 @@ export default function RootLayout({
       <body 
         className={`${geistSans.variable} ${geistMono.variable} bg-neutral min-h-screen text-secondary antialiased`}
       >
-        <AuthGuard>{children}</AuthGuard>
+        {children}
       </body>
     </html>
   );
