c3579ebc4df6cd7c4b1de227ed0a6602f2a988b2
Manning Wu <manningwu07@users.noreply.github.com>
2025-12-02 17:00:09 -0500

    Configring

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Install conda, python=3.11
 Install nvm, version 24.9.0
 
-Backend lives on https://0.0.0.0:8001
+Backend lives on https://0.0.0.0:8000
 Frontend lives on https://localhost:3000
 
 # 🧠 Healthbot Frontend
--- _backend/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .utils_tensorblock import TensorblockClientRunner
-from .utils_openai import OpenAIClientRunner
-from .utils_milvus import MilvusUtil
-
-__all__ = ["TensorblockClientRunner", "OpenAIClientRunner", "MilvusUtil"]
